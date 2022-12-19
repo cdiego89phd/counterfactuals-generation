99b01ebd055e9b1080f436995d6a1f6a44df66c4
dcarraro <diego.carraro@insight-centre.org>
2022-12-19 18:54:17 +0100

    Under development

--- cata_sentiment/generate_training_counterfactuals.py
+++ cata_sentiment/generate_training_counterfactuals.py
@@ -1,4 +1,185 @@
+import pandas as pd
+import numpy as np
+import argparse
+import datetime
+import yaml
+import torch
+import openprompt
+from sentiment_task import generation, utils
+
+from openprompt.prompts import ManualTemplate
+from openprompt.plms.lm import LMTokenizerWrapper
+
 # TODO
 # Generate $m$ corresponding counterfactuals from $D_{seed}$. We use CouRGe*(CouRGe with GPT2 fine-tuned on SFT*).
 # We produce only one counterfactual per data point, and at the end of the generation, we check the LFS, LEV and TED
 # (just to check the quality of the new points).
+
+
+def generate_counterfactuals(yaml_file,
+                             df_testset,
+                             trained_lm,
+                             tokenizer,
+                             gen_params,
+                             n_to_generate=1) -> generation.CounterGenerator:
+
+    special_tokens = yaml_file['SPECIAL_TOKENS']
+    map_labels = yaml_file['MAP_LABELS']
+    generation_prompt = yaml_file['GENERATION_PROMPT']
+
+    # wrap the datasets with the prompt template
+    df_testset["wrapped_input"] = df_testset.apply(lambda row: utils.wrap_dataset_with_prompt(row,
+                                                                                              generation_prompt,
+                                                                                              map_labels,
+                                                                                              special_tokens), axis=1)
+
+    # prepare the data loader
+    test_set = generation.SentimentDataset(raw_dataframe=df_testset.copy(deep=True))
+    test_set.prepare_dataloader()
+
+    template_prompt = '{"placeholder":"text_a"}{"mask"}'
+    prompt_template = ManualTemplate(text=template_prompt, tokenizer=tokenizer)
+    tokenizer_wrapper = LMTokenizerWrapper
+    test_data_loader = openprompt.PromptDataLoader(
+        dataset=list(test_set.get_dataset().values()),
+        tokenizer=tokenizer,
+        template=prompt_template,
+        tokenizer_wrapper_class=tokenizer_wrapper,
+        # truncate_method="tail",
+    )
+
+    counter_generator = generation.CounterGenerator(prompt_template,
+                                                    trained_lm,
+                                                    test_data_loader,
+                                                    test_set,
+                                                    gen_params)
+
+    counter_generator.perform_generation(tokenizer, n_to_generate)
+
+    # the generated counterfactuals are held inside the counter_generator object
+    return counter_generator
+
+
+def append_prompt(parsed_yaml_file, gen_testset, n_to_generate) -> pd.DataFrame:
+    generation_prompt = parsed_yaml_file['GENERATION_PROMPT']
+
+    # parse the prompt
+    generation_prompt = generation_prompt.replace("<", "")
+    generation_prompt = generation_prompt.replace(">", "")
+    parsed_prompt = generation_prompt.split(" ")
+    indexes = [eval(i) for i in parsed_prompt]
+
+    # append the words to the counterfactual
+    for idx in range(n_to_generate):
+        gen_testset[f"generated_counter_{idx}"] = gen_testset.apply(
+            lambda row: append_to_counter(row, indexes, idx), axis=1)
+
+    return gen_testset
+
+
+def append_to_counter(row, idxs, idx) -> str:
+    example = row['example'].split(" ")
+    to_add = ""
+    for i in idxs:
+        to_add += example[i] + " "
+    to_return = to_add + row[f"generated_counter_{idx}"]
+
+    return to_return
+
+
+def main():
+
+    # read params from command line
+    parser = argparse.ArgumentParser()
+    # SETTINGS_PATH = "/home/diego/counterfactuals-generation/cata_sentiment/settings/"
+    parser.add_argument(
+        "--setting_path",
+        default=None,
+        type=str,
+        required=True,
+        help="The absolute path of the file settings."
+    )
+
+    # e.g. SETTING_NAME = "generation_prompt-1.yaml"
+    parser.add_argument(
+        "--setting_name",
+        default=None,
+        type=str,
+        required=True,
+        help="The name of yaml file where to load the setting from."
+    )
+
+    parser.add_argument(
+        "--debug_mode",
+        default=None,
+        type=int,
+        required=True,
+        help="Whether to run the script in debug mode. The script will run with a reduced dataset size."
+    )
+
+    args = parser.parse_args()
+
+    # read params from yaml file
+    setting_yaml_file = open(f"{args.setting_path}{args.setting_name}")
+    parsed_yaml_file = yaml.load(setting_yaml_file, Loader=yaml.FullLoader)
+
+    dataset_path = parsed_yaml_file['DATASET_PATH']
+    lm_name = parsed_yaml_file['LM_NAME']
+    special_tokens = parsed_yaml_file['SPECIAL_TOKENS']
+    n_to_generate = parsed_yaml_file['N_TO_GENERATE']
+
+    # set Random seed
+    torch.manual_seed(parsed_yaml_file["SEED"])
+    np.random.seed(parsed_yaml_file["SEED"])
+
+    print(f"{datetime.datetime.now()}: Begin generation")
+
+    # load the seed dataset
+    df_seed = pd.read_csv(f"{dataset_path}seed_data.csv", sep='\t')
+    if args.debug_mode:
+        df_seed = df_seed[:10]
+    print(f"{datetime.datetime.now()}: Seed data loaded")
+    print(f"# of samples in seed data:{len(df_seed)}")
+
+    # generate counterfactuals from the seed dataset
+    # gen_data = generate()
+    gen_data = df_seed
+
+    # load the n_data dataset
+    # n_data = pd.read_csv(f"{dataset_path}n_data.csv", sep='\t')
+    n_data = df_seed
+
+    # produce training dataset
+    training_data = pd.concat([n_data, gen_data])
+
+    print(f"{datetime.datetime.now()}: Generation completed!")
+
+    tokenizer, trained_lm, _ = utils.load_gpt2_objects(base_lm_name, special_tokens)
+    if parsed_yaml_file['MODEL_FROM_LOCAL']:
+        model_local_path = f"{parsed_yaml_file['MODEL_DIR']}/{parsed_yaml_file['LM_NAME']}"
+        trained_lm = utils.load_gpt2_from_local(model_local_path)
+    print(f"{datetime.datetime.now()}: Language model loaded from local:{parsed_yaml_file['MODEL_FROM_LOCAL']}")
+
+
+    # generate the counterfactuals
+    gen_params = parsed_yaml_file['GEN_CFGS']
+
+    # we generate n_to_generate counterfactuals
+    gen_testset = generate_counterfactuals(parsed_yaml_file, df_testset, trained_lm, tokenizer,
+                                           gen_params, n_to_generate)
+    df_gen_testset = gen_testset.dataframe_from_dataset(n_to_generate)
+
+    print("Generation completed!")
+
+    # print test generation
+    gen_filename = f"{lm_name}{parsed_yaml_file['OUT_LABEL']}.csv"
+    df_gen_testset.to_csv(f"{parsed_yaml_file['OUT_DIR']}{gen_filename}", sep='\t', header=True, index=False)
+
+    print(f"{datetime.datetime.now()}: End GEN TUNING for fold:{fold}")
+
+    return
+
+
+if __name__ == "__main__":
+    main()
+

be9d073acb533bb606edd963754be1ba50636b38
dcarraroHP <diego.carraro@insight-centre.org>
2022-12-20 10:36:35 +0100

    Development of fine tuning classifier done. Beginning of the code testing

--- cata_sentiment/generate_training_counterfactuals.py
+++ cata_sentiment/generate_training_counterfactuals.py
@@ -7,12 +7,6 @@ import torch
 from sentiment_task import generator, utils
 
 
-# TODO
-# Generate $m$ corresponding counterfactuals from $D_{seed}$. We use CouRGe*(CouRGe with GPT2 fine-tuned on SFT*).
-# We produce only one counterfactual per data point, and at the end of the generation, we check the LFS, LEV and TED
-# (just to check the quality of the new points).
-
-
 def append_prompt(parsed_yaml_file, gen_testset, n_to_generate) -> pd.DataFrame:
     generation_prompt = parsed_yaml_file['GENERATION_PROMPT']
 
@@ -122,7 +116,7 @@ def main():
     df_gen.drop(columns=["paired_id", "label_ex", "example"])
     df_gen.rename(columns={"label_counter": "label", "counterfactual": "text"}, inplace=True)
 
-    assert df_gen.columns == n_data.columns
+    # assert df_gen.columns == n_data.columns
     training_data = pd.concat([n_data, df_gen])
 
     # print training data

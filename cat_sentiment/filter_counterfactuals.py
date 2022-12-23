f7cc3d1fc6b763943e96c9c83228064002eeb68b
dcarraroHP <diego.carraro@insight-centre.org>
2022-12-23 18:47:10 +0100

    Implemented filtering of counterfactuals

---
+++ cat_sentiment/filter_counterfactuals.py
@@ -0,0 +1,128 @@
+import datetime
+import argparse
+import pandas as pd
+import transformers
+import numpy as np
+import sys
+import os
+from sentiment_task import evaluation, utils
+
+
+def classify_text(text_to_classify, model, label_map):
+    if len(text_to_classify) > 512:
+        return label_map[model(text_to_classify[:511])[0]['label']]
+    else:
+        return label_map[model(text_to_classify)[0]['label']]
+
+
+def evaluate(args, results_filename):
+    # open the file with the results
+    counter_data = pd.read_csv(f"{args.generation_path}{results_filename}", sep='\t')
+    n_nan = counter_data['generated_counter_0'].isna().sum()
+    print(f"# of nan values removed in generated counterfactuals:{n_nan}")
+    counter_data.dropna(inplace=True)
+
+    if args.debug_mode:
+        counter_data = counter_data[:10]
+        # # TODO remove this
+        # counter_data["generated_counter_1"] = counter_data["generated_counter_0"].values
+        # counter_data["generated_counter_2"] = counter_data["generated_counter_0"].values
+
+    # load classifier
+    classification_tools = utils.prepare_classifier(args.classifier_name)
+
+    # prepare the classifier
+    classifier = transformers.pipeline(
+        task="sentiment-analysis",
+        model=classification_tools['classifier'],
+        tokenizer=classification_tools['tokenizer'],
+        framework="pt",
+        device=0)
+    label_map = classification_tools["label_map"]
+
+    texts = []
+    labels = []
+
+    for _, row in counter_data.iterrows():
+        target_label = row['label_counter']
+        filtered_text = row['generated_counter_0']
+        pred_label = classify_text(filtered_text, classifier, label_map)
+
+        if pred_label != target_label:
+            # look for the following generated counterfactuals
+            for i in range(1, args.n_generated):
+                pred = classify_text(row[f'generated_counter_{i}'], classifier, label_map)
+                if pred == target_label:
+                    filtered_text = row[f'generated_counter_{i}']
+                    pred_label = pred
+                    break
+
+        texts.append(filtered_text)
+        labels.append(pred_label)
+
+    # print filtered counterfactuals
+    d = {"text": texts, "label": labels}
+    filtered_to_print = pd.DataFrame(data=d)
+    filtered_to_print.to_csv(f"{args.generation_path}filtered_counterfactuals_data.csv",
+                             sep='\t', header=True, index=False)
+
+    # print filtered training data
+    n_data = pd.read_csv(f"{args.generation_path}n_data.csv", sep='\t')
+    n_data.drop(columns=["sentiment", "review_len"], inplace=True)
+    training_data = pd.concat([n_data, filtered_to_print])
+    n_nan = training_data['text'].isna().sum()
+    print(f"# of nan values removed in trainset:{n_nan}")
+    training_data.to_csv(f"{args.generation_path}filtered_training_data.csv", sep='\t', header=True, index=False)
+
+
+def main():
+
+    # read params from command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--generation_path",
+        default=None,
+        type=str,
+        required=True,
+        help="The path where results (generated counterfactuals) are stored."
+    )
+
+    parser.add_argument(
+        "--results_filename",
+        default=None,
+        type=str,
+        required=True,
+        help="The filename of the results."
+    )
+
+    parser.add_argument(
+        "--classifier_name",
+        default=None,
+        type=str,
+        required=True,
+        help="The name of the sentiment classifier."
+    )
+
+    parser.add_argument(
+        "--n_generated",
+        default=None,
+        type=int,
+        required=True,
+        help="The # of generated counterfactuals."
+    )
+
+    parser.add_argument(
+        "--debug_mode",
+        default=None,
+        type=int,
+        required=True,
+        help="Whether to run the script in debug mode. The script will run with a reduced dataset size."
+    )
+
+    args = parser.parse_args()
+
+    evaluate(args, args.results_filename)
+
+
+if __name__ == "__main__":
+    main()

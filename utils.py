80abce3f79d947c6623a6c510550d43b95008953
dcarraroHP <diego.carraro@insight-centre.org>
2023-01-19 23:20:12 +0100

    - Implementing gpt-j integration

--- utils.py
+++ utils.py
@@ -89,7 +89,9 @@ def load_tokenizer(tok_name, spec_tokens="None") -> transformers.AutoTokenizer:
 def load_causal_model(model_name: str, n_tokens: int, spec_tokens="None") -> \
         (transformers.AutoModelForCausalLM, transformers.AutoConfig):
     model_config_class = transformers.AutoConfig.from_pretrained(model_name)
-    model = transformers.AutoModelForCausalLM.from_pretrained(model_name)
+    # model = transformers.AutoModelForCausalLM.from_pretrained(model_name)
+    model = transformers.GPTJForCausalLM.from_pretrained(model_name, low_cpu_mem_usage=True)
+
     print("Downloaded model and cfg!")
     if spec_tokens != "None":
         # special tokens added, model needs to be resized accordingly

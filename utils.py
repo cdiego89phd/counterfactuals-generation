c9158209924d645a94c9cb4edf603abb476e659e
dcarraroHP <diego.carraro@insight-centre.org>
2023-01-20 18:47:56 +0100

    - Implementing gpt-j integration

--- utils.py
+++ utils.py
@@ -90,9 +90,10 @@ def load_tokenizer(tok_name, spec_tokens="None") -> transformers.AutoTokenizer:
 def load_causal_model(model_name: str, n_tokens: int, load_in_8bit=False, spec_tokens="None") -> \
         (transformers.AutoModelForCausalLM, transformers.AutoConfig):
     model_config_class = transformers.AutoConfig.from_pretrained(model_name)
-    model = transformers.AutoModelForCausalLM.from_pretrained(model_name,
-                                                              load_in_8bit=load_in_8bit,
-                                                              device_map='sequential')
+    model = transformers.AutoModelForCausalLM.from_pretrained(model_name)
+    # model = transformers.AutoModelForCausalLM.from_pretrained(model_name,
+    #                                                           load_in_8bit=load_in_8bit,
+    #                                                           device_map='sequential')
 
     print("Downloaded model and cfg!")
     if spec_tokens != "None":
